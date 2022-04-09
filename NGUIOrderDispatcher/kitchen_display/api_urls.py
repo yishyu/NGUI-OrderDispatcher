4407314fee34f51ef32270dd40a0f5760ed369e0
yishyu <yishen.yu@ulb.be>
2022-04-09 16:26:45 +0200

    [Del] wtf quote

--- NGUIOrderDispatcher/kitchen_display/api_urls.py
+++ NGUIOrderDispatcher/kitchen_display/api_urls.py
@@ -5,7 +5,7 @@ import kitchen_display.api_views as views
 app_name = 'kitchen_display'
 urlpatterns = [
     path('getqueuedorders', views.getQueuedOrders, name="getQueuedOrders"),
-    path("'changeStateToDone", views.changeStateToDone, name="changeStateToDone"),
+    path("changeStateToDone", views.changeStateToDone, name="changeStateToDone"),
     path('incrementdonequantity', views.incrementDoneQuantity, name="incrementDoneQuantity"),
     path('addneworders', views.addNewOrders, name="addNewOrders"),
     path('getcurrentpreparingorders/<int:shop_pk>', views.getCurrentPreparingOrders, name="getCurrentPreparingOrders"),
